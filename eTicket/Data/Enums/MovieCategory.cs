c623d0a354958ec718f12d42eed902248ee5b521
Mansi <mansi@compunneldigital.com>
2022-06-22 10:38:41 +0530

    Update Enum

--- eTicket/Data/Enums/MovieCategory.cs
+++ eTicket/Data/Enums/MovieCategory.cs
@@ -8,8 +8,8 @@ namespace eTicket.Data
     public enum MovieCategory
     {
         Action=1,
-        Comedy,
-        Drama,
-        Documentary
+        Comedy = 2,
+        Drama = 3,
+        Documentary = 4
     }
 }
