0915d17a8e6aa6d9219508e7b2b6eb24fa8d4330
Mansi <mansi@compunneldigital.com>
2022-07-07 12:53:25 +0530

    cors

--- eTicket/Startup.cs
+++ eTicket/Startup.cs
@@ -82,6 +82,11 @@ namespace eTicket
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            //Enable CORS
+            app.UseCors(options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()); if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
             app.UseHttpsRedirection();
             app.UseAuthentication();
             app.UseStaticFiles();
